cf29d76d49920aceb3dfedbca82ad2d524f375d5
AntoineYu <yzq356350@gmail.com>
2020-04-06 21:39:54 +0200

    Creation

---
+++ src/main/groovy/gl51/service/impl/IpServiceImpl.groovy
@@ -0,0 +1,13 @@
+package gl51.service.impl
+
+import gl51.data.Ip
+import gl51.service.IPService
+
+
+@Singleton
+class IpServiceImpl implements IPService {
+    @Override
+    Ip fetchIp() {
+        return null
+    }
+}
